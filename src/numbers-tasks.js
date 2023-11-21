07cca3f14f616801e31b5f227c466fd0e9a10561
Dmitriy <dm.stebenev@gmail.com>
2023-11-21 12:36:45 +0200

    init: added solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -635,7 +635,7 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -1, 1 => -1 | 0 | 1
  */
 function getRandomInteger(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
   // throw new Error('Not implemented');
 }
 
