987961ec3b3a4874e8df2d8aaf904543e038ccf3
Dmitriy <dm.stebenev@gmail.com>
2023-11-21 08:30:55 +0200

    added task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -355,8 +355,9 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -370,8 +371,9 @@ function getSine(/* num */) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  return number.toString(base);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -384,8 +386,9 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -399,8 +402,9 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -415,8 +419,9 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -429,8 +434,9 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -448,8 +454,9 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return typeof number === 'number' && Number.isFinite(number);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -463,8 +470,9 @@ function isNumber(/* number */) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isFinite(number) && Number.isInteger(number);
+  // throw new Error('Not implemented');
 }
 
 /**
