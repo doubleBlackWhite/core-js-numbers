928a8a42562e5a92d419a0eca70dbccdcb0857af
Dmitriy <dm.stebenev@gmail.com>
2023-11-21 11:36:28 +0200

    init: added solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -341,8 +341,9 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return Math.log2(num) % 1 === 0;
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -485,8 +486,9 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -503,8 +505,10 @@ function getFloatOnString(/* str */) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
+
+  // throw new Error('Not implemented');
 }
 
 /**
